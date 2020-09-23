335630cbc84e849333a2a84f75b3e54221527808
Matteo Borghi <matteosimoneborghi@gmail.com>
2020-09-23 15:59:03 +0200

    converted rating into star

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
-// Ciao ragazzi,
-// esercizio di oggi: Boolflix
-// repo: ajax-ex-boolflix
-// Iscriviamoci al sito https://www.themoviedb.org. E’ completamente gratuito. 
-// Richiediamo la nostra API_KEY che verrà utilizzata in tutte le nostre chiamate. Servirà all’API a capire chi sta effettuando la chiamata.
-// Per richiederla clicchiamo sul nostro user, poi impostazioni, API e clicchiamo su “Richiedi una nuova API key”.
-// Una volta generato, in Impostazioni / API avremo la nostra chiave, indispensabile per tutte le nostre chiamate.
-// Qua https://developers.themoviedb.org/3 troveremo tutte le chiamate possibili all’API. Possiamo giocarci in un secondo momento, ma come prima cosa concentriamoci su Search / Movies.
-// Milestone 1
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato: 
-// Titolo
-// Titolo Originale
-// Lingua
-// Voto
-// Buon lavoro! :slightly_smiling_face:
+// Milestone 2:
+
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+// Qui un esempio di chiamata per le serie tv:
+// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+
 
 $(document).ready(function(){
 
@@ -43,7 +37,7 @@ function searchFilm(keyword){
         success: function(data,state){
             var resultSet = data.results;
             for(var i = 0; i < resultSet.length; i++){
-                // console.log(resultSet[i]);
+                resultSet[i].vote_average = Math.ceil(resultSet[i].vote_average/2);
                 renderFilm(resultSet[i]);
             }
         },
@@ -57,5 +51,16 @@ function searchFilm(keyword){
 //function to render html based on the template from the object recived from the api
 function renderFilm(filmObj){
     var template = Handlebars.compile($("#template").html());
+    var rating = filmObj.vote_average;
+    var star = document.createElement("i");
+
+    star.classList.add("fas");
+    star.classList.add("fa-star");
     $(".film-list").append(template(filmObj));
+
+    var ratingDomObj = $(".film:last-child .vote_average");
+
+    for(var i = 0; i < rating; i++){
+        ratingDomObj.append(star.cloneNode());
+    }
 }
